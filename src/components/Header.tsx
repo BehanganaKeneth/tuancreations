e5a9f04e098a9e7f673cbc3bfbd60031691c9a14
Behangana Keneth <grandeekeneth@gmail.com>
2025-07-18 04:59:03 +0300

    Replace Header Logo with Text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-2">
           <Link to="/" className="flex items-center mr-8">
-            <img src="/logo-black copy.png" alt="TUAN Creations Inc. Logo" className="h-12 w-auto" />
+            <span className="text-2xl font-bold text-white">TUAN Creations Inc.</span>
           </Link>
 
           {/* Desktop Navigation */}
