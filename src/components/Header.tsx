94087903601838d7a2beb69a33233bb3a84a6fc3
Behangana Keneth <grandeekeneth@gmail.com>
2025-07-18 05:05:10 +0300

    Change Header Background to Black

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
   ];
 
   return (
-    <header className="bg-gradient-to-r from-green-600 to-green-800 text-white shadow-lg sticky top-0 z-50">
+    <header className="bg-black text-white shadow-lg sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-2">
           <Link to="/" className="flex items-center mr-8">
